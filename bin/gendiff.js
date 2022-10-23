0ab2ae9a240a2caa1ebca9a75bd6663630f27d4f
Nadezhda <n.d.niki2012@gmail.com>
2022-10-23 22:02:49 +0300

    add "action" to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import generateDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -9,6 +10,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(generateDiff(filepath1, filepath2));
+  });
 
 program.parse();
