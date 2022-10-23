ef1c35b8b079902eb32afbb4c31f74ec52dce554
Nadezhda <n.d.niki2012@gmail.com>
2022-10-23 22:03:25 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+import { cwd } from 'process';
 
-const getDiff = (data1, data2) => {
+const getAbsolutePath = (filepath) => path.resolve(cwd(), filepath);
+const readFile = (path) => fs.readFileSync(path, 'utf-8');
+const getData = (data) => JSON.parse(data);
+
+const generateDiff = (filepath1, filepath2) => {
+  const path1 = getAbsolutePath(filepath1);
+  const path2 = getAbsolutePath(filepath2);
+
+  const fileContent1 = readFile(path1);
+  const fileContent2 = readFile(path2);
+
+  const data1 = getData(fileContent1);
+  const data2 = getData(fileContent2);
+
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
 
@@ -33,4 +49,4 @@ const getDiff = (data1, data2) => {
   return `{\n${newDifferences.join('\n')}\n}`;
 };
 
-export default getDiff;
+export default generateDiff;
