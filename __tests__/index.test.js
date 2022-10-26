508f2c06b922c32bd75eef97dbb38d1ce5d8dc0a
Nadezhda <n.d.niki2012@gmail.com>
2022-10-26 22:08:52 +0300

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,18 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
 import generateDiff from '../src/index.js';
 
-const filepath1 = 'fixtures/file1.json';
-const filepath2 = 'fixtures/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const filepath3 = '/home/kaktus89/frontend-projects/frontend-project-46/fixtures/file1.json';
-const filepath4 = 'fixtures/file2.json';
+const getFixturePath = (filename) => path.join(__dirname, '..', 'fixtures', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const result = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+const filepath1 = getFixturePath('file1.json');
+const filepath2 = getFixturePath('file2.json');
+const expectedResult = readFile('result.json').trim();
 
-test ('difference', () => {
-    expect(generateDiff(filepath1, filepath2)).toEqual(result);
-    expect(generateDiff(filepath3, filepath4)).toEqual(result);
+test('differences', () => {
+  expect(generateDiff(filepath1, filepath2)).toEqual(expectedResult);
 });
