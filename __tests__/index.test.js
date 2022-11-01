9cd3760d37d7ebaba0c526af35139ea791905317
Nadezhda <n.d.niki2012@gmail.com>
2022-11-01 20:57:11 +0300

    fix linter error

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -15,7 +15,7 @@ test('differences', () => {
   const filepath3 = getFixturePath('file3.yml');
   const filepath4 = getFixturePath('file4.yaml');
   const expectedResult = readFile('result.txt').trim();
-  
+
   expect(generateDiff(filepath1, filepath2)).toEqual(expectedResult);
   expect(generateDiff(filepath3, filepath4)).toEqual(expectedResult);
 });
