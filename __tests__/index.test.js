f3bf15013564b6b5c0ed7147ff4095675d43145f
Nadezhda Nikitina <109433215+Nadezhda-97@users.noreply.github.com>
2022-11-01 20:24:47 +0300

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,13 +9,13 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const filepath1 = getFixturePath('file1.json');
-const filepath2 = getFixturePath('file2.json');
-const filepath3 = getFixturePath('file3.yml');
-const filepath4 = getFixturePath('file4.yaml');
-const expectedResult = readFile('result.yml').trim();
-
 test('differences', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const filepath3 = getFixturePath('file3.yml');
+  const filepath4 = getFixturePath('file4.yaml');
+  const expectedResult = readFile('result.txt').trim();
+  
   expect(generateDiff(filepath1, filepath2)).toEqual(expectedResult);
   expect(generateDiff(filepath3, filepath4)).toEqual(expectedResult);
 });
