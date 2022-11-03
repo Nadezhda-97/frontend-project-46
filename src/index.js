6f2abee8f4c082253986bc45a3d03097392bb755
Nadezhda Nikitina <109433215+Nadezhda-97@users.noreply.github.com>
2022-11-03 23:13:37 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
-import { cwd } from 'process';
+import cwd from 'process';
 import parse from './parsers.js';
+import buildTree from './buildTree.js';
 
-const generateDiff = (filepath1, filepath2) => {
+const generateDiff = (filepath1, filepath2, format = 'stylish') => {
   const path1 = path.resolve(cwd(), filepath1);
   const path2 = path.resolve(cwd(), filepath2);
 
@@ -16,37 +16,9 @@ const generateDiff = (filepath1, filepath2) => {
 
   const data1 = parse(content1, format1);
   const data2 = parse(content2, format2);
+  const differences = buildTree(data1, data2);
 
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-
-  const keys = [...keys1, ...keys2];
-  const sorted = _.sortBy(_.uniq(keys));
-
-  const differences = sorted.map((key) => {
-    const result = [];
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      result.push(`  - ${key}: ${data1[key]}`);
-    }
-
-    if (_.has(data1, key) && _.has(data2, key)) {
-      if (data1[key] === data2[key]) {
-        result.push(`    ${key}: ${data1[key]}`);
-      } else {
-        result.push(`  - ${key}: ${data1[key]}`);
-        result.push(`  + ${key}: ${data2[key]}`);
-      }
-    }
-
-    if (!_.has(data1, key) && _.has(data2, key)) {
-      result.push(`  + ${key}: ${data2[key]}`);
-    }
-
-    return result;
-  });
-
-  const newDifferences = differences.flat();
-  return `{\n${newDifferences.join('\n')}\n}`;
+  return differences; //update this later
 };
 
 export default generateDiff;
