a7f54f62ad266964e5d5e25acede763273177862
Nadezhda Nikitina <109433215+Nadezhda-97@users.noreply.github.com>
2022-11-03 22:59:30 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,7 +12,7 @@ program
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(generateDiff(filepath1, filepath2));
+    console.log(generateDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
