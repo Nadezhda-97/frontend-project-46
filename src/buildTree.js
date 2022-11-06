fa86f2bd16159daad1da926f3eeb0b45a343fdb6
Nadezhda Nikitina <109433215+Nadezhda-97@users.noreply.github.com>
2022-11-06 20:57:24 +0300

    fix buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import stylish from '../formatters/stylish.js';
 
 const buildTree = (data1, data2) => {
   const keys1 = Object.keys(data1);
@@ -7,27 +8,27 @@ const buildTree = (data1, data2) => {
 
   const result = keys.map((key) => {
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return { key, type: 'nested', children: buildTree(data1[key], data2[key]) };
+      return { name: key, type: 'nested', children: buildTree(data1[key], data2[key]) };
     }
 
     if (!_.has(data1, key)) {
-      return { key, type: 'added', value: data2[key] };
+      return { name: key, type: 'added', value: data2[key] };
     }
 
     if (!_.has(data2, key)) {
-      return { key, type: 'deleted', value: data1[key] };
+      return { name: key, type: 'deleted', value: data1[key] };
     }
 
     if (!_.isEqual(data1[key], data2[key])) {
-      return { key, type: 'changed', value1: data1[key], value2: data2[key] };
+      return { name: key, type: 'changed', value1: data1[key], value2: data2[key] };
     }
 
-    return { key, type: 'unchanged', value: data1[key] };
+    return { name: key, type: 'unchanged', value: data1[key] };
 
   });
 
   return result;
-  };
+};
   
-  export default buildTree;
+export default buildTree;
   
