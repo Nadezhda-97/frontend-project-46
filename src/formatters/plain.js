c951b9e4ec5502a5e19adedc08236149789f74ae
Nadezhda <n.d.niki2012@gmail.com>
2022-11-08 22:43:51 +0300

    update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,5 +1,32 @@
+import _ from 'lodash';
+
+const buildString = (data) => {
+  if (_.isObject(data)) {
+    return '[complex value]';
+  }
+
+  return typeof data === 'string' ? `'${data}'` : String(data);
+};
+
 const plain = (data) => {
-  return data; //update this later
+  const result = data
+    .filter((diff) => diff.type !== 'unchanged')
+    .flatMap((diff) => {
+      switch (diff.type) {
+        case 'added':
+          return `Property '${diff.name}' was added with value: ${buildString(diff.value)}`;
+        case 'deleted':
+          return `Property '${diff.name}' was removed`;
+        case 'changed':
+          return `Property '${diff.name}' was updated. From ${buildString(diff.value1)} to ${buildString(diff.value2)}`;
+        case 'nested':
+          return plain(diff.children);
+        default:
+          throw new Error(`Unknown type: ${diff.type}`);
+      }
+    });
+
+  return `${result.join('\n')}`;
 };
 
 export default plain;
